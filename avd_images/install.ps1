e9cddb6d49cb7a33ace03fe6b77bcc45fa581e71
JohannesWeizenegger <johannes@Win-1H845221F2.>
2025-06-11 17:21:35 +0200

    update

--- avd_images/install.ps1
+++ avd_images/install.ps1
@@ -1,6 +1,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+$env:Path += ";C:\ProgramData\chocolatey\bin"
+
 
 choco install greenshot -y --accept-package-agreements --accept-source-agreements *> C:\install_greenshot.log
 
