99c9dad12a38afb3b82fff8a66d65822330c39f4
JohannesWeizenegger <johannes@Win-1H845221F2.>
2025-06-11 13:34:13 +0200

    add agreement

--- avd_images/install.ps1
+++ avd_images/install.ps1
@@ -2,7 +2,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install greenshot -y --verbose --debug
-choco install 7zip.install -y --verbose --debug
+choco install greenshot -y --accept-package-agreements --accept-source-agreements *> C:\install_greenshot.log
+
+choco install 7zip.install -y --accept-package-agreements --accept-source-agreements *> C:\install_7zip.log
 
 
