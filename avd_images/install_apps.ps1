85ed86b71f090ac0a99b0cd2843bf8bb3424d3d2
JohannesWeizenegger <johannes@Win-1H845221F2.>
2025-06-12 09:09:15 +0200

    update

--- avd_images/install_apps.ps1
+++ avd_images/install_apps.ps1
@@ -2,6 +2,6 @@
 # choco install 7zip.install -y --accept-package-agreements --accept-source-agreements
 
 # Chocolatey direkt über vollständigen Pfad aufrufen, damit es sicher funktioniert:
-& "C:\ProgramData\chocolatey\bin\choco.exe" install greenshot -y --accept-package-agreements --accept-source-agreements
+#& "C:\ProgramData\chocolatey\bin\choco.exe" install greenshot -y --accept-package-agreements --accept-source-agreements
 
 & "C:\ProgramData\chocolatey\bin\choco.exe" install 7zip.install -y --accept-package-agreements --accept-source-agreements
