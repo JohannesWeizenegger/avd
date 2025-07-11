b3620716e4b3122d412f2d2d829f8cf0f7d514d4
JohannesWeizenegger <johannes@Win-1H845221F2.>
2025-07-11 10:38:34 +0200

    update

--- avd_images/install_apps.ps1
+++ avd_images/install_apps.ps1
@@ -14,7 +14,7 @@ reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters /v CloudKe
 
 net use z: \\$STORAGE_ACCOUNT_NAME.file.core.windows.net\$SHARE_NAME $STORAGE_ACCESS_KEY /user:Azure\$STORAGE_ACCOUNT_NAME
 
-icacls z: /grant "DOMMULTIVAC\RA_AAD_AVD.FSLogix:(M)"
+icacls z: /grant "DOMMULTIVAC\DL_OS_DE0.CIT.ISSC:(M)"
 icacls z: /grant "Creator Owner:(OI)(CI)(IO)(M)"
 icacls z: /remove "Authenticated Users"
 icacls z: /remove "Builtin\Users"
