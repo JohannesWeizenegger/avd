98d0f1759f1e9cb823f8e1ba16778713bdd4ec10
JohannesWeizenegger <johannes@Win-1H845221F2.>
2025-07-11 10:09:20 +0200

    update

--- avd_images/install_apps.ps1
+++ avd_images/install_apps.ps1
@@ -1,2 +1,21 @@
-#& "C:\ProgramData\chocolatey\bin\choco.exe" install 7zip -y --accept-package-agreements --accept-source-agreements
+# install applications
 choco install 7zip -y --accept-package-agreements --accept-source-agreements
+
+
+
+param(
+[string]$STORAGE_ACCESS_KEY
+[string]$STORAGE_ACCOUNT_NAME
+[string]$SHARE_NAME
+)
+
+# Prepare VM for FSLogix
+reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters /v CloudKerberosTicketRetrievalEnabled /t REG_DWORD /d 1
+
+net use z: \\$STORAGE_ACCOUNT_NAME.file.core.windows.net\$SHARE_NAME $STORAGE_ACCESS_KEY /user:Azure\$STORAGE_ACCOUNT_NAME
+
+icacls z: /grant "DOMMULTIVAC\RA_AAD_AVD.FSLogix:(M)"
+icacls z: /grant "Creator Owner:(OI)(CI)(IO)(M)"
+icacls z: /remove "Authenticated Users"
+icacls z: /remove "Builtin\Users"
+
